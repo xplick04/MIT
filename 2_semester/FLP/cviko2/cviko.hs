93ce835f3539250045f464b82519d40e7d155660
xplick04 <xplick04@fit.vutbr.cz>
2024-04-07 13:42:52 +0200

    FLP

--- 2_semester/FLP/cviko2/cviko.hs
+++ 2_semester/FLP/cviko2/cviko.hs
@@ -1,11 +1,12 @@
 --zipWith (\x y -> (x,y)) [1..10] [1..10] --zip s pomoci zipWith
 
-
+{-
 filter1 :: (a -> Bool) -> [a] -> [a]
 filter1 f = foldr (\x acc -> if f x then x:acc else acc) []
 
 data Tree a = EmptyTree | Node a (Tree a) (Tree a)
-
+-}
+{-
 treeInsert :: (Ord a) => a -> Tree a -> Tree a
 treeInsert a EmptyTree = Node a EmptyTree EmptyTree
 treeInsert a (Node b l r)
@@ -18,36 +19,51 @@ treeElem a (Tree a Tree l Tree r)
     | a > b = treeElem a l
     | a < b = treeElem a r
     | True = True
+-}
+
+data Tree k v = EmptyTree | Node k v (Tree k v) (Tree k v) deriving (Show)
+
+kTreeAdd :: (Ord k) => (k, v) -> Tree k v -> Either String (Tree k v) 
+kTreeAdd (k, v) EmptyTree = Right (Node k v EmptyTree EmptyTree)
+kTreeAdd x@(k, v) (Node kN vN left right)
+    | k < kN = case kTreeAdd x left of
+                 Left err -> Left err
+                 Right newLeft -> Right (Node kN vN newLeft right)
+    | k > kN = case kTreeAdd x right of
+                 Left err -> Left err
+                 Right newRight -> Right (Node kN vN left newRight)
+    | otherwise = Left "TralalTrololo"
+
+removeRight (Right a) = a
 
 
-data KTree k v
-    = EmptyTree
-    | KNode k v (KTree k v) (KTree k v)
-    deriving (Show)
+--kTreeAdd (key, value) 
 
-KtreeInsert :: (Ord k) => k -> KTree k v -> KTree k v
-treeInsert a EmptyTree = Node a EmptyTree EmptyTree
-ktreeInsert k EmptyTree _ = (KNode k0 v0 EmptyTree EmptyTree)
-KtreeInsert k (KNode k0 v0 Ktree k1 k1 Ktree k2 v2)
-    | k < k0 =  KtreeInsert (k1 k1)
-    | k > k0 =  KtreeInsert (k2 k2)
-    | True = Left "."
 
+{-
 KtreeElem :: (Ord k) => k -> KTree k v -> Mabye v
 ktreeElem k EmptyTree _ = Nothing
 KtreeElem k (KNode k0 v0 Ktree k1 k1 Ktree k2 v2)
     | k < k0 =  KtreeElem (k1 k1)
     | k > k0 =  KtreeElem (k2 k2)
     | k == k0 = Just 
+-}
 
-
-
+{-
 instance (Show a) => Show (Tree a) where
     Show t = "--" ++ helper t ++ "--"
     where 
         helper EmptyTree = ""
         helper (Node a Tree l Tree r) = show x ++ helper l ++ helper r
+-}
+
+
+instance Functor (Tree k) where
+    fmap _ EmptyKtree = EmptyKtree
+    fmap f (Knode k v left right) = Knode k (f v) (fmap f left) (fmap f right)
 
 
 main :: IO ()
-main = do print()
+main = do 
+    print("nic")
+    
