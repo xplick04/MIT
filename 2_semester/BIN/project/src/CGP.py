95fca3118c16537bedf6500aec4d0b0d220a746a
xplick04 <maxim.plicka@icloud.com>
2024-04-30 17:09:54 +0200

    ROC curve

--- 2_semester/BIN/project/src/CGP.py
+++ 2_semester/BIN/project/src/CGP.py
@@ -4,6 +4,8 @@ import numpy as np
 from sklearn.model_selection import KFold
 from tqdm import tqdm
 import wandb
+from sklearn.metrics import roc_curve, auc
+import matplotlib.pyplot as plt
 
 STATISTICS = False
 
@@ -48,36 +50,31 @@ class Individual:
     def execute(self):
         for i in range(config['x_size']):
             for j in range(config['y_size']):
-                for k in range(self.outputValues.shape[0]):
-                    gate = (self.chromozome[(i * config['y_size'] + j) * 3], self.chromozome[(i * config['y_size'] + j) * 3 + 1], self.chromozome[(i * config['y_size'] + j) * 3 + 2])
+                #for k in range(self.outputValues.shape[0]):
+                gate = (self.chromozome[(i * config['y_size'] + j) * 3], self.chromozome[(i * config['y_size'] + j) * 3 + 1], self.chromozome[(i * config['y_size'] + j) * 3 + 2])
 
-                    if gate[2] == 0: # addition
-                        self.outputValues[k][(i * config['y_size'] + j) + input_size] = self.outputValues[k][gate[0]] + self.outputValues[k][gate[1]]
+                if gate[2] == 0: # addition
+                    self.outputValues[:, (i * config['y_size'] + j) + input_size] = self.outputValues[:, gate[0]] + self.outputValues[:, gate[1]]
 
-                    elif gate[2] == 1: # subtraction
-                        self.outputValues[k][(i * config['y_size'] + j) + input_size] = self.outputValues[k][gate[0]] - self.outputValues[k][gate[1]]
+                elif gate[2] == 1: # subtraction
+                    self.outputValues[:, (i * config['y_size'] + j) + input_size] = self.outputValues[:, gate[0]] - self.outputValues[:, gate[1]]
 
-                    elif gate[2] == 2: # multiplication
-                        self.outputValues[k][(i * config['y_size'] + j) + input_size] = self.outputValues[k][gate[0]] * self.outputValues[k][gate[1]]
+                elif gate[2] == 2: # multiplication
+                    self.outputValues[:, (i * config['y_size'] + j) + input_size] = self.outputValues[:, gate[0]] * self.outputValues[:, gate[1]]
 
-                    elif gate[2] == 3: # division by zero
-                        if self.outputValues[k][gate[1]] == 0:
-                            self.outputValues[k][(i * config['y_size'] + j) + input_size] = 0
-                        else:
-                            self.outputValues[k][(i * config['y_size'] + j) + input_size] = self.outputValues[k][gate[0]] / self.outputValues[k][gate[1]]
+                elif gate[2] == 3: # division 
+                    # Define a small constant
+                    small_constant = 0.001
+                    # Find the indexes where the value is 0
+                    zero_indexes = np.where(self.outputValues[:, gate[1]] == 0)
+                    # Replace the values at those indexes with the small constant
+                    self.outputValues[zero_indexes, gate[1]] = small_constant
+                    self.outputValues[:, (i * config['y_size'] + j) + input_size] = self.outputValues[:, gate[0]] / self.outputValues[:, gate[1]]
 
-        # get thresholded output values
-        treshold = 0
-        for k in range(self.outputValues.shape[0]):
-
-            if self.outputValues[k][self.chromozome[-1]] > treshold:
-                self.outputValues[k][-1] = 1
-            else:
-                self.outputValues[k][-1] = 0
-
-        return self.outputValues[:,-1] # return vector of outputs
-
-
+        
+        # Apply sigmoid activation function to the output
+        self.outputValues[:, -1] = 1 / (1 + np.exp(-self.outputValues[:, self.chromozome[-1]]))
+        return self.outputValues[:, -1]  # return vector of outputs
 
     def mutate(self):
         num_mutations = random.randint(0, config['mut_max'])
@@ -141,11 +138,18 @@ class Population:
             error = np.linalg.norm(result - self.labels)  # Compute the error between result vector and labels
             fitness.append(error)
         return fitness
+    
+    def evaluate2(self):
+        fitness = []
+        for individual in self.population:
+            result = individual.execute()  # Assuming execute returns a vector of results (one training data result per row)
+            squared_diff = np.square(result - self.labels)  # Compute the squared differences
+            mse = np.mean(squared_diff)  # Calculate the mean squared error
+            fitness.append(mse)
+        return fitness
 
     def train(self):
         for g in tqdm(range(config['num_generations']), desc="Training"):
-            """if g == config['num_generations'] // 2:
-                config['mut_max'] //= 2""" # should i divide like in simulating annealing?
 
             fitness = self.evaluate()
 
@@ -160,10 +164,12 @@ class Population:
             for individual in self.population[1:]:
                 individual.mutate()
 
+            
             if STATISTICS:
                 wandb.log({"best_fitness": best_fitness})
             else:
                 print(f"Generation {g}, Best fitness: {best_fitness:.2f}")
+            
 
         self.best_individual = self.population[0]
 
@@ -174,8 +180,29 @@ class Population:
         test_individual = Individual(test_outputValues)
         test_individual.chromozome = self.best_individual.chromozome
         test_result = test_individual.execute()
+        test_result = np.where(test_result > 0.5, 1, 0) # thresholding
         test_accuracy = (test_labels == test_result).sum() / len(test_labels)
+
+        #ROC_curve(test_labels, test_result)
         return test_accuracy
+
+
+def ROC_curve(test_labels, test_result):
+    fpr, tpr, thresholds = roc_curve(test_labels, test_result)
+    roc_auc = auc(fpr, tpr)
+
+    plt.figure()
+    lw = 2
+    plt.plot(fpr, tpr, color='darkorange', lw=lw, label='ROC curve (area = %0.2f)' % roc_auc)
+    plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver Operating Characteristic (ROC) Curve')
+    plt.legend(loc="lower right")
+    plt.show()
+
 
 
 def get_data(data, idx):
@@ -190,6 +217,7 @@ def get_data(data, idx):
 
 
 def cross_validation(data, num_generations, pop_size, MUTATION_MAX, lookback, x_size, y_size):
+
 
     global config
     config = {
@@ -206,7 +234,7 @@ def cross_validation(data, num_generations, pop_size, MUTATION_MAX, lookback, x_
         run_name = f"CGP_gens{config['num_generations']}_popSize{config['pop_size']}_MUT{config['mut_max']}_lookback{config['lookback']}_dims({config['x_size']},{config['y_size']})"
         wandb.init(project='BIN-CGP', entity='maxim-pl', name=run_name, config=config)
     test_accs = []
-    kf = KFold(n_splits=10, shuffle=True)
+    kf = KFold(n_splits=5, shuffle=True)
     # K fold cross validation
     for foldID, (train_index, test_index) in enumerate(kf.split(data)):
         print(f"---Fold {foldID + 1}---")
@@ -219,12 +247,43 @@ def cross_validation(data, num_generations, pop_size, MUTATION_MAX, lookback, x_
         pop = Population(config['pop_size'], train_features, train_labels)
         pop.train()
         test_acc = pop.test(test_features, test_labels)
+
         test_accs.append(test_acc)
         if STATISTICS:
             wandb.log({"test_accuracy": test_acc})
         else:
             print(f"Test accuracy: {test_acc*100:.2f}%")
 
+
     if STATISTICS:
         wandb.log({"average_test_accuracy": sum(test_accs) / len(test_accs)})
         wandb.finish()
+    else:
+        print(f"Average test accuracy: {sum(test_accs) / len(test_accs) * 100:.2f}%")
+
+"""
+def testos():
+    data = [[1,2,3,4,5], [5,4,3,2,1]]
+    data = np.array(data)
+    label = [1, 1]
+    label = np.array(label)
+
+    global input_size
+    input_size = data.shape[1]
+    global config
+    config = {
+        "num_generations": 1,
+        "pop_size": 2,
+        "mut_max": 2,
+        "lookback": 2,
+        "x_size": 2,
+        "y_size": 2
+    }
+
+    p = Population(2, data, label)
+    p.train()
+
+
+
+if __name__ == "__main__":
+    testos()"""
